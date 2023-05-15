b3d43934034bf8ddbd68f6a9dfc112263c282082
oh-yes-0-fps <foryt.bowan@gmail.com>
2023-05-15 01:28:37 -0400

    added nt tunnability to constants

--- src/main/java/com/igknighters/constants/ConstValues.java
+++ src/main/java/com/igknighters/constants/ConstValues.java
@@ -2,6 +2,7 @@ package com.igknighters.constants;
 
 import com.igknighters.constants.ConstantHelper.ConstClass;
 import com.igknighters.constants.ConstantHelper.StringConst;
+import com.igknighters.constants.ConstantHelper.TunnableIgnore;
 
 //this will be where we put references to all our initialized values
 public class ConstValues {
@@ -11,12 +12,11 @@ public class ConstValues {
     // this is how you access the constants on a per subsystem basis
     @ConstClass
     public static class kExample {
+        @TunnableIgnore
         public static int WIDTH = 26;
         public static int LENGTH = 26;
 
         @StringConst(yin = "yin", yang = "yang")
         public static String ROBOT_NAME = null;
     }
-
-    
 }
--- src/main/java/com/igknighters/constants/ConstantHelper.java
+++ src/main/java/com/igknighters/constants/ConstantHelper.java
@@ -7,6 +7,12 @@ import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 
 import com.igknighters.constants.RobotSetup.RobotConst;
+import com.igknighters.util.logging.TunnableValuesAPI;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class ConstantHelper {
 
@@ -44,16 +50,28 @@ public class ConstantHelper {
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target({ ElementType.TYPE })
-    public @interface ConstClass {
-    }
+    public @interface ConstClass {}
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target({ ElementType.TYPE, ElementType.FIELD })
+    public @interface TunnableIgnore {}
 
     public static void applyRoboConst(Class<ConstValues> consts) {
+        NetworkTable ntConst;
+        if (ConstValues.DEBUG) {
+            ntConst = NetworkTableInstance.getDefault().getTable("Constants");
+        }
         RobotConst constID = RobotSetup.getRobotID().constID;
         for (Class<?> cls : consts.getDeclaredClasses()) {
-            System.out.println(cls.getName());
+            boolean clsIsTunnable = !cls.isAnnotationPresent(TunnableIgnore.class);
             if (cls.isAnnotationPresent(ConstClass.class)) {
+                NetworkTable ntSubTable = null;
+                if (ConstValues.DEBUG && clsIsTunnable) {
+                    ntSubTable = ntConst.getSubTable(cls.getSimpleName());
+                }
                 for (Field field : cls.getDeclaredFields()) {
                     field.setAccessible(true);
+                    boolean fieldIsTunnable = !field.isAnnotationPresent(TunnableIgnore.class);
                     if (field.isAnnotationPresent(IntConst.class)) {
                         try {
                             IntConst annotation = field.getAnnotation(IntConst.class);
@@ -62,6 +80,7 @@ public class ConstantHelper {
                             } else if (constID == RobotConst.YANG) {
                                 field.set(consts, annotation.yang());
                             }
+                            // entry.setInteger((Long) field.get(consts));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
@@ -73,6 +92,7 @@ public class ConstantHelper {
                             } else if (constID == RobotConst.YANG) {
                                 field.set(consts, annotation.yang());
                             }
+                            // entry.setDouble((double) field.get(consts));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
@@ -84,6 +104,7 @@ public class ConstantHelper {
                             } else if (constID == RobotConst.YANG) {
                                 field.set(consts, annotation.yang());
                             }
+                            // entry.setString((String) field.get(consts));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
@@ -95,9 +116,35 @@ public class ConstantHelper {
                             } else if (constID == RobotConst.YANG) {
                                 field.set(consts, annotation.yang());
                             }
+                            // entry.setBoolean((boolean) field.get(consts));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
+                    }
+                    boolean tunnable = (ConstValues.DEBUG && clsIsTunnable && fieldIsTunnable);
+                    if ((field.getType().isPrimitive() || field.getType() == String.class) && tunnable) {
+                        NetworkTableEntry entry = ntSubTable.getEntry(field.getName());
+                        try {
+                            entry.setValue(field.get(consts));
+                        } catch (IllegalAccessException e) {
+                            DriverStation.reportError("Error setting value for " + cls.getName() + "." + field.getName(), false);
+                        }
+                        TunnableValuesAPI.addTunnableRunnable(() -> {
+                            try {
+                                Class<?> type = field.getType();
+                                if (type == int.class) {
+                                    field.setInt(consts, (int) entry.getInteger(0));
+                                } else if (type == double.class) {
+                                    field.setDouble(consts, entry.getDouble(0));
+                                } else if (type == String.class) {
+                                    field.set(consts, entry.getString(""));
+                                } else if (type == boolean.class) {
+                                    field.setBoolean(consts, entry.getBoolean(false));
+                                }
+                            } catch (IllegalAccessException e) {
+                                DriverStation.reportError("Error setting value for " + cls.getName() + "." + field.getName(), false);
+                            }
+                        });
                     }
                 }
             }
